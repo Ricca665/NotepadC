daf91cbe726b9e8fee76563e4eceb465fb6e129a
Ricca665 <84286914+Ricca665@users.noreply.github.com>
2023-08-29 09:23:18 +0200

    Updated notepad.cpp to be v2 version

--- notepad.cpp
+++ notepad.cpp
@@ -7,9 +7,31 @@ using namespace std;
 
 int main() {
     string text;
+    string name;
+    cout << "inserisci il nome del testo qui sotto con l'estensione .txt (eg. nome.txt)";
+    getline(cin, name);
+    if (name == "con.txt") {
+        cout << "Impossibile scrivere nel file";
+        return 1;
+    } else if (name == "aux.txt") {
+        cout << "Impossibile scrivere nel file";
+        return 1;
+    } else if (name == "prn.txt") {
+        cout << "Impossibile scrivere nel file";
+        return 1;
+    } else if (name == "con") {
+        cout << "Impossibile scrivere nel file";
+        return 1;
+    } else if (name == "aux") {
+        cout << "Impossibile scrivere nel file";
+        return 1;
+    } else if (name == "prn") {
+        cout << "Impossibile scrivere nel file";
+        return 1;
+    }
+    ofstream file(name);
     cout << "inserisci del testo qui sotto plz e quando hai finito di scrivere premi invio plz pls plz pls" << endl;
     getline(cin, text);
-    ofstream file("notepad.txt");
     file << text;
     return 0;
 }
