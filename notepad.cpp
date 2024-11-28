a66faa226f10c90d196bd6c02d2c05bfe5a6c297
Ricca665 <84286914+Ricca665@users.noreply.github.com>
2024-11-28 16:12:42 +0100

    Update notepad.cpp
    Changed a bit the language and grammar corrections

--- notepad.cpp
+++ notepad.cpp
@@ -8,29 +8,29 @@ using namespace std;
 int main() {
     string text;
     string name;
-    cout << "inserisci il nome del testo qui sotto con l'estensione .txt (eg. nome.txt)";
+    cout << "Insert name with .txt extension (eg. name.txt)";
     getline(cin, name);
     if (name == "con.txt") {
-        cout << "Impossibile scrivere nel file";
+        cout << "Can't write to file";
         return 1;
     } else if (name == "aux.txt") {
-        cout << "Impossibile scrivere nel file";
+        cout << "Can't write to file";
         return 1;
     } else if (name == "prn.txt") {
-        cout << "Impossibile scrivere nel file";
+        cout << "Can't write to file";
         return 1;
     } else if (name == "con") {
-        cout << "Impossibile scrivere nel file";
+        cout << "Can't write to file";
         return 1;
     } else if (name == "aux") {
-        cout << "Impossibile scrivere nel file";
+        cout << "Can't write to file";
         return 1;
     } else if (name == "prn") {
-        cout << "Impossibile scrivere nel file";
+        cout << "Can't write to file";
         return 1;
     }
     ofstream file(name);
-    cout << "inserisci del testo qui sotto plz e quando hai finito di scrivere premi invio plz pls plz pls" << endl;
+    cout << "Insert text down here, when finished press enter" << endl;
     getline(cin, text);
     file << text;
     return 0;
